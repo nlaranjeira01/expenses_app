8f9f7531775ca4eebeb00e2962a9922f5bf7fb9a
Nelson <nlaranjeira01@gmail.com>
2020-02-25 14:47:40 -0300

    Added TransactionList to diplay all the transactions in a ListView widget

---
+++ lib/widgets/transaction_list.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import '../models/transaction.dart';
+
+class TransactionList extends StatelessWidget {
+  final List<Transaction> transactionList;
+
+  TransactionList(this.transactionList);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 450,
+      child: ListView.builder(
+        itemBuilder: (BuildContext ctx, int index) => Card(
+          child: Row(
+            children: <Widget>[
+              Container(
+                margin: EdgeInsets.symmetric(
+                  vertical: 10,
+                  horizontal: 15,
+                ),
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.purple,
+                    width: 2,
+                  ),
+                ),
+                padding: EdgeInsets.all(10),
+                child: Text(
+                  "R\$ ${transactionList[index].amount.toString()}",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                    color: Colors.purple,
+                  ),
+                ),
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    transactionList[index].title,
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    DateFormat.yMMMd('pt_BR')
+                        .format(transactionList[index].date),
+                    style: TextStyle(
+                      color: Colors.grey,
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+        itemCount: transactionList.length,
+      ),
+    );
+  }
+}
